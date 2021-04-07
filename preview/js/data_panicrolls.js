b1fd625287718fcaf50d9a7310f4d9c9f79169aa
CaveDann <74264942+CaveDann@users.noreply.github.com>
2021-04-07 09:03:08 +0100

    Replace Movement with Panic Rolls

---
+++ preview/js/data_panicrolls.js
@@ -0,0 +1,116 @@
+data_movement = [
+    {
+        title: "1 - 6",
+        icon: "run",
+        subtitle: "Keeping it Together",
+        description: "Result: 1-6. Keeping it Together.",
+        reference: "Core Rulebook, pg. 105.",
+        bullets: [
+            "You manage to keep your nerves in check. Barely."
+        ]
+    },
+    {
+        title: "7",
+        icon: "crags",
+        subtitle: "Nervous Twitch",
+        description: "Result: 7. Nervous Twitch.",
+        reference: "Core Rulebook, pg. 105.",
+        bullets: [
+            "Your stress level increases by one."
+	    "The stress level of all friendly PCs in short range of you increases by one."
+        ]
+    },
+    {
+        title: "8",
+        icon: "at-sea",
+        subtitle: "Tremble",
+        description: "Result: 8. Tremble.",
+        reference: "Core Rulebook, pg. 105.",
+        bullets: [
+            "You start to tremble uncontrollably. All skill rolls using Agility suffer a -2 modification until your panic stops."
+        ]
+    },
+    {
+        title: "9",
+        icon: "falling",
+        subtitle: "Drop Item",
+        description: "Result: 9. Drop Item.",
+        reference: "Core Rulebook, pg. 105.",
+        bullets: [
+            "Your stress level increases by one."
+	    "Whether by stress, confusion or the realization that you're all going to die anyway, you drop a weapon or other important item - the GM decides which one."
+	]
+    },
+    {
+        title: "10",
+        icon: "crawl",
+        subtitle: "Freeze",
+        description: "Result: 10. Freeze.",
+        reference: "Core Rulebook, pg. 105.",
+        bullets: [
+	    "You're frozen by fear or stress for one round. You lose your next slow action."
+	    "Your stress level increases by one."
+    	    "The stress level of all PCs in short range of you increases by one."
+        ]
+    },
+    {
+        title: "11",
+        icon: "strong",
+        subtitle: "Seek Cover",
+        description: "Result: 11. Seek Cover.",
+        reference: "Core Rulebook, pg. 105.",
+        bullets: [
+            "You must use your next action to move away from danger and find a safe spot if possible. You are allowed to make a retreat roll (pg. 93) if you have an enemy at engaged range."
+ 	    "Your stress level decreases by one."
+ 	    "The stress level of all PCs in short range of you increases by one."
+	    "You can act normally again after one round."
+	]
+    },
+    {
+        title: "12",
+        icon: "wingfoot",
+        subtitle: "Scream",
+        description: "Result: 12. Scream.",
+        
+        reference: "Core Rulebook, pg. 105.",
+        bullets: [
+            "You scream your lungs out for one round, losing your next slow action."
+	    "Your stress level is decreased by one."
+	    "Every friendly character who hears your scream must make an immediate Panic Roll."
+	]
+    },
+    {
+        title: "13",
+        icon: "wingfoot",
+        subtitle: "Flee",
+        description: "Result: 13. Flee.",
+        reference: "Core Rulebook, pg. 105.",
+        bullets: [
+            "You can't take it anymore. You must flee to a safe place and refuse to leave it."
+	    "You won't attack anyone and won't attempt anything dangerous. You are not allowed to make a retreat roll (pg. 93) if you have an enemy at engaged range when you flee."
+	    "Your stress level decreases by one."
+	    "Every friendly character who sees you run must make an immediate Panic Roll."
+	]
+    },
+    {
+        title: "14",
+        icon: "juggler",
+        subtitle: "Berserk",
+	reference: "Core Rulebook, pg. 105."
+        description: "Result: 14. Berserk.",
+        bullets: [
+            "You must immediately attack the nearest person or creature, friendly or not. You won't stop until your target is broken."
+	    "Every friendly character who witnesses your rampage must make an immediate Panic Roll."
+	]
+    },
+    {
+        title: "15+",
+        icon: "stone-pile",
+        subtitle: "Catatonic",
+        reference: "Core Rulebook, pg. 105.",
+        description: "Result: 15. Catatonic.",
+        bullets: [
+            "You collapse to the floor and can't talk or move, staring blankly into oblivion."
+	]
+    }
+]
