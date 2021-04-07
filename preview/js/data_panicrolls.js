fd283594e7d88b0d592cd20edbd41b5ed7fc716b
CaveDann <74264942+CaveDann@users.noreply.github.com>
2021-04-07 09:24:19 +0100

    Add commas to bullet lists

--- preview/js/data_panicrolls.js
+++ preview/js/data_panicrolls.js
@@ -16,7 +16,7 @@ data_panicrolls = [
         description: "Result: 7. Nervous Twitch.",
         reference: "Core Rulebook, pg. 105.",
         bullets: [
-            "Your stress level increases by one."
+            "Your stress level increases by one.",
 	    "The stress level of all friendly PCs in short range of you increases by one."
         ]
     },
@@ -37,7 +37,7 @@ data_panicrolls = [
         description: "Result: 9. Drop Item.",
         reference: "Core Rulebook, pg. 105.",
         bullets: [
-            "Your stress level increases by one."
+            "Your stress level increases by one.",
 	    "Whether by stress, confusion or the realization that you're all going to die anyway, you drop a weapon or other important item - the GM decides which one."
 	]
     },
@@ -48,8 +48,8 @@ data_panicrolls = [
         description: "Result: 10. Freeze.",
         reference: "Core Rulebook, pg. 105.",
         bullets: [
-	    "You're frozen by fear or stress for one round. You lose your next slow action."
-	    "Your stress level increases by one."
+	    "You're frozen by fear or stress for one round. You lose your next slow action.",
+	    "Your stress level increases by one.",
     	    "The stress level of all PCs in short range of you increases by one."
         ]
     },
@@ -60,9 +60,9 @@ data_panicrolls = [
         description: "Result: 11. Seek Cover.",
         reference: "Core Rulebook, pg. 105.",
         bullets: [
-            "You must use your next action to move away from danger and find a safe spot if possible. You are allowed to make a retreat roll (pg. 93) if you have an enemy at engaged range."
- 	    "Your stress level decreases by one."
- 	    "The stress level of all PCs in short range of you increases by one."
+            "You must use your next action to move away from danger and find a safe spot if possible. You are allowed to make a retreat roll (pg. 93) if you have an enemy at engaged range.",
+ 	    "Your stress level decreases by one.",
+ 	    "The stress level of all PCs in short range of you increases by one.",
 	    "You can act normally again after one round."
 	]
     },
@@ -74,8 +74,8 @@ data_panicrolls = [
         
         reference: "Core Rulebook, pg. 105.",
         bullets: [
-            "You scream your lungs out for one round, losing your next slow action."
-	    "Your stress level is decreased by one."
+            "You scream your lungs out for one round, losing your next slow action.",
+	    "Your stress level is decreased by one.",
 	    "Every friendly character who hears your scream must make an immediate Panic Roll."
 	]
     },
@@ -86,9 +86,9 @@ data_panicrolls = [
         description: "Result: 13. Flee.",
         reference: "Core Rulebook, pg. 105.",
         bullets: [
-            "You can't take it anymore. You must flee to a safe place and refuse to leave it."
-	    "You won't attack anyone and won't attempt anything dangerous. You are not allowed to make a retreat roll (pg. 93) if you have an enemy at engaged range when you flee."
-	    "Your stress level decreases by one."
+            "You can't take it anymore. You must flee to a safe place and refuse to leave it.",
+	    "You won't attack anyone and won't attempt anything dangerous. You are not allowed to make a retreat roll (pg. 93) if you have an enemy at engaged range when you flee.",
+	    "Your stress level decreases by one.",
 	    "Every friendly character who sees you run must make an immediate Panic Roll."
 	]
     },
@@ -99,7 +99,7 @@ data_panicrolls = [
 	reference: "Core Rulebook, pg. 105."
         description: "Result: 14. Berserk.",
         bullets: [
-            "You must immediately attack the nearest person or creature, friendly or not. You won't stop until your target is broken."
+            "You must immediately attack the nearest person or creature, friendly or not. You won't stop until your target is broken.",
 	    "Every friendly character who witnesses your rampage must make an immediate Panic Roll."
 	]
     },
